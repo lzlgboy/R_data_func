cd0b7b725ef1c6361dd0363b21f0249657811a4a
kenian <chenkn007@gmail.com>
2017-07-30 12:50:35 -0500

    test

--- PCAplt.R
+++ PCAplt.R
@@ -1,1 +1,6 @@
-PCAplt <- function(df = data.frame )
+PCAplt <- function(df = data.frame,pc=c(1,2),title="",sizeShape=3) {
+    require(ggplot2)
+    require(RColorBrewer)
+    require(ggrepel)
+    data <- t(apply(df,1,scale)) # scale the data
+}

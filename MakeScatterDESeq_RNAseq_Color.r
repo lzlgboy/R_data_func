5eef58c2e31c0263f05f77e06c0deae9d1b4e874
lzlgboy <lzlgboy@users.noreply.github.com>
2018-12-14 13:44:57 -0600

    Update MakeScatterDESeq_RNAseq_Color.r

--- MakeScatterDESeq_RNAseq_Color.r
+++ MakeScatterDESeq_RNAseq_Color.r
@@ -1,9 +1,15 @@
 
-MakeScatterDESeq <- function(df, x=df$baseMean_x, y = df$baseMean_y, xy_lab= c("X","Y"),flag_logged = FALSE,flag_anno = FALSE, col_anno = c(), FC_threshold = 1.5, padj_threshold=0.05) {
+MakeScatterDESeq <- function(df, x=df$baseMean_x, y = df$baseMean_y, xy_lab= c("X","Y"), anno_FC_threshold = 20, anno_padj_threshold = 0.01, col_anno = c(), FC_threshold = 2, padj_threshold=0.05) {
 
     
 require(ggplot2)
 require(ggrepel)
+
+color_pal_1 <- c(
+		 "#009D9A","#00A299","#00A698","#00AB96","#00B095","#00B492","#00B890","#00BC8D", 
+		 "#00C089","#0AC486","#31C881","#47CB7D","#59CF78","#69D273","#77D46E","#85D768", 
+		 "#93DA62","#A0DC5C","#ADDE56","#B9E050","#C5E14A","#D1E245","#DDE340","#E8E43C", 
+		 "#F3E438","#FDE333")
 	
 df.plot <- as.data.frame(cbind(as.data.frame(x),as.data.frame(y)))
 
@@ -53,22 +59,40 @@ lab.NoChange <- paste("NoChange: ",count.NoChange)
 
 legend_title <- paste("FC: ",FC_threshold,"\n","Padj: ",padj_threshold,sep="")
 
-# for color setting
+# for plot setting
 df.plot$Alpha[df.plot$Change == "NoChange"] <- 0 
-df.plot$padj[df.plot$Change == "NoChange"] <- 1 
+
+df.plot$pointSize <- 0.01
+#df.plot$pointSize[df.plot$Change == "NoChange"] <- 0.01
+#df.plot$pointSize[df.plot$Change != "NoChange" & -log10(df.plot$padj) < 10] <- 0.1
+#df.plot$pointSize[df.plot$Change != "NoChange" & -log10(df.plot$padj) >= 10 & -log10(df.plot$padj) < 50] <- 0.3
+#df.plot$pointSize[df.plot$Change != "NoChange" & -log10(df.plot$padj) >= 50 & -log10(df.plot$padj) < 100] <- 0.5
+#df.plot$pointSize[df.plot$Change != "NoChange" & -log10(df.plot$padj) >= 100 & -log10(df.plot$padj) < 200] <- 1
+#df.plot$pointSize[df.plot$Change != "NoChange" & -log10(df.plot$padj) >= 200 ] <- 2
+
+
+#df.plot$padj[df.plot$Change == "NoChange"] <- 1 
 df.plot <- df.plot[df.plot$Change != "NoChange",] 
 df.plot$Alpha[df.plot$Change == "Up"] <- 0.5 
 df.plot$Alpha[df.plot$Change == "Down"] <- 0.5 
+	
+# for anno text
+#log2_anno_FC_threshold <- log2(anno_FC_threshold)
+#df.plot$Row.names[df$padj > anno_padj_threshold || abs(df.plot$log2FoldChange) < anno_FC_threshold] <- ""
 
 ggplot(df.plot,aes(x=log(df.plot[,1]+1,2),y=log(df.plot[,2]+1,2))) +
         geom_point(aes(alpha=Alpha,color=abs(log2FoldChange),size=-log10(padj))) +
+        #geom_point(aes(alpha=Alpha,color=abs(log2FoldChange),size=pointSize)) +
+
         geom_abline(linetype = "dashed",slope=1) +
         
         theme_bw() +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-        scale_color_gradientn(colours=pal(70)) +
+        scale_color_gradientn(colours=color_pal_1) +
         labs(colour=legend_title) + 
         
         xlab(xy_lab[1]) +
-        ylab(xy_lab[2]) 
+        ylab(xy_lab[2])
 }
+
+

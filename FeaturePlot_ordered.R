d069ae35dbdc1c3de81500e6ab3674f7cef08ae1
lzlgboy <lzlgboy@users.noreply.github.com>
2019-01-31 11:47:10 -0600

    Create FeaturePlot_ordered.R

---
+++ FeaturePlot_ordered.R
@@ -0,0 +1,11 @@
+# First, the usual command to make the feature plots and return the ggplot2 objects
+fp <- Seurat::FeaturePlot(object=p, features.plot=GeneList, cols.use = c("gray75", "red"), 
+                          min.cutoff="q25",max.cutoff="q75", reduction.use = "tsne", 
+                          do.return=TRUE)
+
+# Here, fp is a list of objects, one per feature plot
+
+# Then, re-order the "data" dataframe in increasing order of gene expressions
+for (i in 1:length(fp)){
+  fp[[i]]$data <- fp[[i]]$data[order(fp[[i]]$data$gene),]
+}

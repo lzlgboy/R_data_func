3936272639e3d7b2b43191a26eff9ec7cf4315ac
lzlgboy <lzlgboy@users.noreply.github.com>
2021-02-20 11:53:00 -0600

    Update Split_PDX_HumanMouse_scRNA.R

--- Split_PDX_HumanMouse_scRNA.R
+++ Split_PDX_HumanMouse_scRNA.R
@@ -41,8 +41,11 @@ Split_HumanMouse_10X_Count <- function( RawCount ,pct_threshold = 0.05) {
     numMouseCell <- length(Mouse_Count[1,])
     numHumanCell <- length(Human_Count[1,])
     numDoublet   <- length(df$Human) - numMouseCell - numHumanCell
+    
+    doublet_rate = df$NumDoublet / (df$NumMouseCell + df$NumHumanCell + df$NumDoublet)
 
-    returnList <- list("HumanCount" = Human_Count, "MouseCount" = Mouse_Count, "plot"=plt, "plot_hist"=plt_hist,"NumDoublet"=numDoublet,"NumMouseCell"=numMouseCell,"NumHumanCell"=numHumanCell)
+
+    returnList <- list("HumanCount" = Human_Count, "MouseCount" = Mouse_Count, "doubletRate"=doublet_rate,"plot"=plt, "plot_hist"=plt_hist,"NumDoublet"=numDoublet,"NumMouseCell"=numMouseCell,"NumHumanCell"=numHumanCell)
     
     return(returnList)
 }

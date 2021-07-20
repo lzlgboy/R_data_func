1ce3588abea4032b37fd2e1d88ddd298fcc70ce5
lzlgboy <lzlgboy@users.noreply.github.com>
2021-07-20 14:36:19 -0500

    Update VolcanoPlot.R

--- VolcanoPlot.R
+++ VolcanoPlot.R
@@ -1,5 +1,5 @@
 VolcanoPlot <- function (toptable, lab, x, y, selectLab = NULL, xlim = c(min(toptable[, 
-  x], na.rm = TRUE), max(toptable[, x], na.rm = TRUE)), ylim = c(0, 
+  x], na.rm = TRUE)-0.1, max(toptable[, x], na.rm = TRUE)+0.1), ylim = c(-0.1, 
   max(-log10(toptable[, y]), na.rm = TRUE) + 5), xlab = bquote(~Log[2] ~ 
   "fold change"), ylab = bquote(~-Log[10] ~ italic(P)), axisLabSize = 16, 
   pCutoff = 0.05, pLabellingCutoff = pCutoff, FCcutoff = 2, 

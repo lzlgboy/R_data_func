ff0c18974e990d276aeeb998a866e6b756434d4b
lzlgboy <lzlgboy@users.noreply.github.com>
2025-03-05 14:38:33 -0600

    Create GREAT_Func.R

---
+++ GREAT_Func.R
@@ -0,0 +1,16 @@
+GREAT_func <- function(bed_grange = bed_grange, genome = 'hg38') { 
+    print("Perfroming GREAT analysis!!!")
+    
+    
+    result <- list()
+    suppressWarnings({
+    job.GREAT <- submitGreatJob(bed.Up.iHCO2.vs.iHCO.FC4,species = 'hg38')
+    result$table.GO.Molecular.Function = getEnrichmentTables(job.GREAT,ontology = "GO Molecular Function")
+    result$table.GO.Biological.Process = getEnrichmentTables(job.GREAT,ontology = "GO Biological Process")
+    result$table.GO.Cellular.Component = getEnrichmentTables(job.GREAT,ontology = "GO Cellular Component")
+    result$table.Mouse.Phenotype = getEnrichmentTables(job.GREAT,ontology = "Mouse Phenotype")
+    result$table.HumanP.henotype = getEnrichmentTables(job.GREAT,ontology = "Human Phenotype")
+    })
+        
+    return(result)
+}
